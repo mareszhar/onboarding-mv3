be7d93b89d3c651215c72c3e6b5589bd65722c36
Mares <66750744+mareszhar@users.noreply.github.com>
2021-10-15 12:49:03 -0400

    scripts are not reinjected in SPAs

--- background.js
+++ background.js
@@ -1,35 +1,32 @@
-
-try{
-//On first install open onboarding
-chrome.runtime.onInstalled.addListener(r => {
-  if(r.reason == 'install'){
-    //first install
-    //show onboarding page
-    chrome.tabs.create({
-      url: 'onboarding-page.html'
-    });
-  };
-});
-
-
-//ON page change
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  //chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) { // <-- can use to grab tabId if not within tabs listener...
-    //const tabId = tabs[0].id; // set tabid
-  
-    // read changeInfo data and do something with it (like read the url)
-    if (changeInfo.url) {
-      //if have all_urls permissions...
-      chrome.scripting.executeScript({
-        files: ['contentScript.js'],
-        target: {tabId: tabId}
+try {
+  //On first install open onboarding
+  chrome.runtime.onInstalled.addListener((r) => {
+    if (r.reason == "install") {
+      //first install
+      //show onboarding page
+      chrome.tabs.create({
+        url: "onboarding-page.html",
       });
     }
-  
-  //}); // <-- close extra listener for tabid
-});
+  });
 
+  //ON page change
+  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    //chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) { // <-- can use to grab tabId if not within tabs listener...
+    //const tabId = tabs[0].id; // set tabid
 
-}catch(e){
+    // read changeInfo data and do something with it (like read the url)
+    if (changeInfo.url) {
+      console.log(`url is ${changeInfo.url}`);
+      //if have all_urls permissions...
+      chrome.scripting.executeScript({
+        files: ["contentScript.js"],
+        target: { tabId: tabId },
+      });
+    }
+
+    //}); // <-- close extra listener for tabid
+  });
+} catch (e) {
   console.log(e);
 }
