34818af3099e03890ff5c4ab9286fe7857992b3e
Mares <66750744+mareszhar@users.noreply.github.com>
2021-10-15 21:08:22 -0400

    found that all urls listed in host permissions is not considered optional by default, but required

--- onboarding.js
+++ onboarding.js
@@ -1,5 +1,59 @@
 const button = document.querySelector("button");
-button.addEventListener("click", (event) => {
+button.addEventListener("click", async () => {
+  const permissionsOfInterest = {
+    origins: ["<all_urls>"],
+    permissions: ["tabs"],
+  };
+
+  const permissionsExist = await checkPermissions(permissionsOfInterest);
+
+  if (permissionsExist) {
+    alert("you already have those permissions");
+    const permissionsRemoved = await removePermissions(permissionsOfInterest);
+    if (permissionsRemoved) {
+      alert("but now you don't");
+    } else {
+      alert("you're a tough one, arent you?");
+    }
+  } else {
+    const permissionsGranted = await requestPermissions(permissionsOfInterest);
+    if (permissionsGranted) {
+      alert("thanks!");
+    } else {
+      alert("ouch, why not?");
+    }
+  }
+
+  // chrome.permissions.request({
+  //   origins: ["<all_urls>"],
+  // });
+});
+
+function requestPermissions(permissionsRequest) {
+  return new Promise((resolve) => {
+    chrome.permissions.request(permissionsRequest, (permissionsGranted) => {
+      resolve(permissionsGranted);
+    });
+  });
+}
+
+function removePermissions(permissionsRemoval) {
+  return new Promise((resolve) => {
+    chrome.permissions.remove(permissionsRemoval, (permissionsRemoved) => {
+      resolve(permissionsRemoved);
+    });
+  });
+}
+
+function checkPermissions(permissions) {
+  return new Promise((resolve) => {
+    chrome.permissions.contains(permissions, (permissionsExist) => {
+      resolve(permissionsExist);
+    });
+  });
+}
+
+/*
   chrome.permissions.contains(
     {
       origins: ["<all_urls>"],
@@ -29,32 +83,4 @@ button.addEventListener("click", (event) => {
       }
     }
   );
-
-  chrome.permissions.request({
-    origins: ["<all_urls>"],
-  });
-});
-
-function requestPermission(permissionsRequest) {
-  return new Promise((resolve) => {
-    chrome.permissions.request(permissionsRequest, (permissionsGranted) => {
-      resolve(permissionsGranted);
-    });
-  });
-}
-
-function removePermission(permissionsRemoval) {
-  return new Promise((resolve) => {
-    chrome.permissions.remove(permissionsRemoval, (permissionsRemoved) => {
-      resolve(permissionsRemoved);
-    });
-  });
-}
-
-function checkPermissions(permissions) {
-  return new Promise((resolve) => {
-    chrome.permissions.contains(permissions, (permissionsExist) => {
-      resolve(permissionsExist);
-    });
-  });
-}
+*/
