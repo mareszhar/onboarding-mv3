8fa3fcb46bea539253701c1e48dfe0d506aacc78
Mares <66750744+mareszhar@users.noreply.github.com>
2021-10-15 21:00:00 -0400

    added async friendly wrappers of common permission methods

--- onboarding.js
+++ onboarding.js
@@ -34,3 +34,27 @@ button.addEventListener("click", (event) => {
     origins: ["<all_urls>"],
   });
 });
+
+function requestPermission(permissionsRequest) {
+  return new Promise((resolve) => {
+    chrome.permissions.request(permissionsRequest, (permissionsGranted) => {
+      resolve(permissionsGranted);
+    });
+  });
+}
+
+function removePermission(permissionsRemoval) {
+  return new Promise((resolve) => {
+    chrome.permissions.remove(permissionsRemoval, (permissionsRemoved) => {
+      resolve(permissionsRemoved);
+    });
+  });
+}
+
+function checkPermissions(permissions) {
+  return new Promise((resolve) => {
+    chrome.permissions.contains(permissions, (permissionsExist) => {
+      resolve(permissionsExist);
+    });
+  });
+}
